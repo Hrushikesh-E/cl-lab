60e6d8c4948cdc75c6c2825b76e9a9e803ec573b
Hrushikesh-E <158154658+Hrushikesh-E@users.noreply.github.com>
2024-05-06 09:12:40 +0530

    Delete lab-8 directory

--- lab-8/audio.m
+++
@@ -1,5 +0,0 @@
-input_file = "C:/Users/Hrushi'/Documents/Computational lab/lab-8/Chorus.wav"
-output_file = "C:/Users/Hrushi'/Documents/Computational lab/lab-8/wave.wav"
-[y,fs] = audioread(input_file);
-reversedata = flipud(y);
-audiowrite(output_file, reversedata, fs);
